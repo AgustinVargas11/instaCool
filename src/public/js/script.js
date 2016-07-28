f70526b59e4673f42e4a5e6e094119d5d76b0226
Agustin Vargas <Agustinvargas11@gmail.com>
2016-07-27 22:39:45 -0400

    update
    update script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,4 +1,18 @@
-if (document.getElementById('head').innerHTML.length > 12) {
-	document.getElementById('head').className += "shrink";
+'use strict';
+
+if (window.location.pathname !== '/') {
+	if (document.getElementById('head').innerHTML.length > 12) {
+		document.getElementById('head').className += "shrink";
 	}
-	
+}
+
+document.getElementById('search').addEventListener('click', function(e) {
+	e.preventDefault();
+	console.log(document.getElementById('userName').innerText);
+	if (document.getElementById('userName').value.length === 0) {
+		document.getElementById('userName').className = "warn";
+	} else {
+		this.parentNode.submit();
+		document.getElementById('userName').className = '';
+	}
+});
